d3c5954e430a6afca8acda6943cfab2bc7df696d
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-07-25 12:59:43 +0200

    Added TrelloConnector script and inital source code to connector

---
+++ src/js/connector.js
@@ -0,0 +1,8 @@
+console.log('Hello World!');
+
+window.TrelloPowerUp.initialize({
+    'card-badges': function(t, options){
+        //return an array at card-badges 
+        return [];
+    }
+});
