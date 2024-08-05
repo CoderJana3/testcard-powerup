4f814387ac8c3b84f625096c687875c59853c214
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-08-05 12:03:48 +0200

    changed link to icon

--- src/js/connector.js
+++ src/js/connector.js
@@ -4,7 +4,8 @@ window.TrelloPowerUp.initialize({
     'card-buttons': function(t, options){
         //return an array at card-badges 
         return [{
-            icon: 'https://github.com/CoderJana3/testcard-powerup/blob/main/src/testicon.png',
+            icon:'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+            // icon: 'https://github.com/CoderJana3/testcard-powerup/blob/main/src/testicon.png',
             text: 'Estimate Size',
         }];
     }
